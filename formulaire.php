40b9ef4ee7033a9ff83888b796a1233589510b75
EthanLorant <ethan.lorant@sts-sio-caen.info>
2023-12-24 14:02:50 +0100

    update

--- formulaire.php
+++ formulaire.php
@@ -1,18 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+ if {
+      
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;                   
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.gmail.com';                  
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'ethanlorant@gmail.com';                  
+    $mail->Password   = 'fomg izrw fujz jhwz';                               
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+    $mail->Port = 465;                                   
 
-    $to = "ethan.lorant@sts-sio-caen.info"; // Remplacez ceci par votre adresse email
+    
+    $mail->setFrom($email, $objet);
+    $mail->addAddress('ethan.lorant@sts-sio-caen.info'); 
 
-    $subject = "Nouveau message de $name";
-    $body = "Nom: $name\nEmail: $email\nMessage: $message";
+    $mail->isHTML(true);
+    $mail->Subject = $email;
+    $mail->Body = $message;
 
-    if (mail($to, $subject, $body)) {
-        echo "Votre message a été envoyé avec succès.";
-    } else {
-        echo "Une erreur s'est produite lors de l'envoi du message.";
-    }
-}
+    
+    $mail->send();
+
+    
+    echo "Mail envoyé !";
+    exit();
+ }
 ?>
